fc70d07f3257d09d1795ce4cc05ac85f6caba170
mac company <m15577822957@163.com>
2019-11-25 15:24:13 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    "react/jsx-filename-extension": "off",
+    "react/forbid-prop-types": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/interactive-supports-focus": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "react/no-array-index-key": "off",
+    "react/jsx-props-no-spreading": "off",
+    "no-console": "off"
+  },
+  env: {
+    browser: true
+  },
+};
